b3109e17b0431c912c5789dc542ceb4fd429ded7
Jazzmoin <jazzypas@icloud.com>
2025-06-24 13:16:54 +1000

    flower resizing works

--- src/flower.rs
+++ src/flower.rs
@@ -1,20 +1,18 @@
-use std::ops::Range;
+use std::cmp::max;
 use crate::*;
 use nannou;
 use nannou::prelude::*;
 
 use std::time::Instant;
-use nannou::glam::cast::Vec2Cast;
-use rand::Rng;
 
 #[derive(Clone, Debug)]
 pub(crate) struct FlowerGene {
     pub(crate) centre_size: f32,
+    pub(crate) centre_dist: f32,
     pub(crate) centre_color: Srgb<u8>,
     pub(crate) num_petals: usize,
     pub(crate) petal_radius: f32,
     pub(crate) petal_color: Srgb<u8>,
-    pub(crate) centre_dist: f32,
     pub(crate) bloom_duration: f32,
 }
 
@@ -22,11 +20,11 @@ impl Default for FlowerGene {
     fn default() -> Self {
         FlowerGene {
             centre_size: 40.0,
+            centre_dist: 40.0,
             centre_color: Srgb::new(210, 181, 64),
             num_petals: 5,
             petal_radius: 25.0,
             petal_color: FLORALWHITE,
-            centre_dist: 40.0,
             bloom_duration: 5.0,
         }
     }
@@ -37,7 +35,7 @@ pub(crate) struct Flower {
     pub(crate) gene: FlowerGene,
     pub(crate) pos: Vec2,
     pub(crate) start_time: Instant,
-    pub(crate) orientation: f32
+    pub(crate) orientation: f32,
 }
 
 impl Flower {
@@ -50,12 +48,12 @@ impl Flower {
         }
     }
 
-    pub fn new(pos: Vec2, gene: FlowerGene, random_orientation: f32) -> Self {
+    pub fn new(pos: Vec2, gene: FlowerGene, orientation: f32) -> Self {
         Self {
             gene,
             pos,
             start_time: Instant::now(),
-            orientation: random_orientation,
+            orientation,
         }
     }
 
@@ -64,37 +62,19 @@ impl Flower {
         (1.0 - (1.0 - x).powi(3)).clamp(0.0, 1.0)
     }
 
-    pub fn dist_to_other_flower(&self, other: &Flower) -> f32 {
-        self.pos.distance(other.pos)
-    }
-
-    pub fn surface_dist_to_other_flower(&self, other: &Flower) -> f32 {
-        self.pos.distance(other.pos)
-            - (self.gene.petal_radius + self.gene.centre_size)
-            - (other.gene.petal_radius + other.gene.centre_size)
-            + 50.0
-    }
-
-    pub fn is_touching_other_flower(&self, other: &Flower) -> bool {
-        self.surface_dist_to_other_flower(other) <= 0.0
-    }
-
-    pub fn shift_flowers(&mut self, other: &mut Flower) {
-        if self.pos == other.pos {
-            let angle = random::<f32>() * TAU;
-            self.pos += Vec2::new(angle.cos(), angle.sin()) * 0.01;
-        }
-        let dist = self.surface_dist_to_other_flower(other);
-        if dist < 0.0 {
-            self.pos -= (self.pos - other.pos).normalize() * dist / 1.98;
-            other.pos -= (other.pos - self.pos).normalize() * dist / 1.98;
-        }
-    }
-    
     pub fn radius(&self) -> f32 {
         self.gene.centre_dist + self.gene.petal_radius
     }
     
+    pub fn max_radius(new_pos: Vec2, others: &[Flower]) -> f32 {
+        others
+            .iter()
+            .map(|other| other.pos.distance(new_pos) - other.radius())
+            .fold(f32::INFINITY, f32::min)
+            .max(0.0)
+    }
+    // return option or add an assert when creating a flower
+
     pub fn draw(&self, draw: &Draw, current_time: &Instant) {
         let elapsed = current_time.duration_since(self.start_time).as_secs_f32();
         for petal in 0..self.gene.num_petals {
@@ -103,33 +83,34 @@ impl Flower {
 
             let petal_radius = self.bloom_progress(elapsed) * self.gene.petal_radius;
             let distance_from_centre = self.gene.centre_dist * self.bloom_progress(elapsed);
-            
-            let p = self.pos + Vec2::new(petal_angle.cos(), petal_angle.sin()) * distance_from_centre;
-            
+
+            let p =
+                self.pos + Vec2::new(petal_angle.cos(), petal_angle.sin()) * distance_from_centre;
+
             draw.ellipse()
                 .xy(p)
-                .wh(Vec2::new(1.0,0.6) * petal_radius * 2.0)
+                .wh(Vec2::new(1.0, 0.6) * petal_radius * 2.0)
                 .rotate(petal_angle)
                 .color(self.gene.petal_color);
         }
 
         let centre_size = self.bloom_progress(elapsed) * self.gene.centre_size;
         draw.ellipse()
-            .x_y(self.pos.x, self.pos.y)
-            .w_h(centre_size, centre_size)
+            .xy(self.pos)
+            .wh(Vec2::splat(centre_size))
             .color(self.gene.petal_color);
-         
+
         draw.ellipse()
-            .x_y(self.pos.x, self.pos.y)
-            .w_h(centre_size / 1.5, centre_size / 1.5)
+            .xy(self.pos)
+            .wh(Vec2::splat(centre_size) / 1.5)
             .color(self.gene.centre_color);
-        
+
+        // debug cirle
         // draw.ellipse()
         //     .xy(self.pos)
-        //     .wh(Vec2::splat(self.radius()))
-        //     .rgba8(0,0,0,0)
+        //     .radius(self.radius())
+        //     .no_fill()
         //     .stroke(RED)
-        //     .stroke_weight(2.0);
-        
+        //     .stroke_weight(1.0);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -48,20 +48,7 @@ fn setup(app: &App) -> Model {
     }
 }
 
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    for i in 0..model.flowers.len() {
-        for j in (i + 1)..model.flowers.len() {
-            let (flower_1, flower_2) = {
-                let (left, right) = model.flowers.split_at_mut(j);
-                (&mut left[i], &mut right[0])
-            };
-
-            if flower_1.is_touching_other_flower(flower_2) {
-                Flower::shift_flowers(flower_2, flower_1)
-            }
-        }
-    }
-}
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn event(app: &App, model: &mut Model, event: WindowEvent) {
     match event {
@@ -72,57 +59,54 @@ fn event(app: &App, model: &mut Model, event: WindowEvent) {
                 }
                 VirtualKeyCode::Key2 => {
                     model.current_gene = FlowerGene {
-                        centre_size: 40.0,
-                        centre_color: Srgb::new(210, 181, 64),
-                        num_petals: 10,
-                        petal_color: WHITESMOKE,
-                        petal_radius: 50.0,
-                        centre_dist: 40.0,
+                        centre_size: 50.0,
+                        centre_dist: 50.0,
+                        centre_color: Srgb::new(95, 34, 21),
+                        num_petals: 9,
+                        petal_radius: 40.0,
+                        petal_color: Srgb::new(239, 191, 18),
                         bloom_duration: 7.0,
                     }
                 }
                 VirtualKeyCode::Key3 => {
                     model.current_gene = FlowerGene {
                         centre_size: 50.0,
-                        centre_color: Srgb::new(95, 34, 21),
-                        num_petals: 9,
-                        petal_color: Srgb::new(239, 191, 18),
-                        petal_radius: 50.0,
-                        centre_dist: 40.0,
-                        bloom_duration: 7.0,
-                    }
-                }
-                VirtualKeyCode::Key4 => {
-                    model.current_gene = FlowerGene {
-                        centre_size: 50.0,
-                        centre_color: Srgb::new(95, 34, 21),
+                        centre_dist: 50.0,
+                        centre_color: Srgb::new(247, 225, 148),
                         num_petals: 6,
+                        petal_radius: 40.0,
                         petal_color: Srgb::new(201, 165, 201),
-                        petal_radius: 50.0,
-                        centre_dist: 40.0,
                         bloom_duration: 7.0,
                     }
                 }
-                VirtualKeyCode::Key5 => {}
-                VirtualKeyCode::Key6 => {}
-                VirtualKeyCode::Key7 => {}
-                VirtualKeyCode::Key8 => {}
-                VirtualKeyCode::Key9 => {}
                 _ => {}
             }
         }
         MousePressed(_) => {
             let mouse_position = app.mouse.position();
-            let random_orientation = random::<f32>() * TAU;
-            let new_flower = Flower::new(mouse_position, model.current_gene.clone(), random_orientation);
-            
-            model.flowers.push(new_flower);
+            let orientation = random::<f32>() * TAU;
+            let max_radius = Flower::max_radius(mouse_position, &model.flowers);
 
-            // Todo: mutate model.flower_gene which will result in the next flower being different.
-            let mutation_val = 2.0;
-            model.current_gene.centre_size += mutation_val;
-            model.current_gene.centre_dist += mutation_val;
-            model.current_gene.petal_radius += mutation_val;
+            let initial_radius = model.current_gene.centre_dist + model.current_gene.petal_radius;
+            let scale = (max_radius / initial_radius).min(1.0);
+            
+            if scale < 0.25 {
+                return
+            }
+
+            let mut scaled_flower = model.current_gene.clone();
+            scaled_flower.centre_size *= scale;
+            scaled_flower.centre_dist *= scale;
+            scaled_flower.petal_radius *= scale;
+            
+            let new_flower = Flower::new(mouse_position, scaled_flower, orientation);
+            model.flowers.push(new_flower);
+            
+            // // Todo: mutate model.flower_gene which will result in the next flower being different.
+            // let mutation_val = 2.0;
+            // model.current_gene.centre_size += mutation_val;
+            // model.current_gene.centre_dist += mutation_val;
+            // model.current_gene.petal_radius += mutation_val;
         }
         _ => {}
     }
